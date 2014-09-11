20765a86ce04d98455af545bb6baac6406f079c8
Nick McLaughlin <mclaughlin.nick@gmail.com>
2014-09-11 10:52:17 -0400

    step 6 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911143015) do
+ActiveRecord::Schema.define(version: 20140911143929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20140911143015) do
     t.datetime "updated_at"
     t.string   "author",     null: false
     t.integer  "rating"
+  end
+
+  create_table "checkouts", force: true do |t|
+    t.integer  "book_id",    null: false
+    t.string   "reader",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
